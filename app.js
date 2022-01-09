7799e1014eb3acc2089371e4202f437e31a8f191
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-10 02:53:50 +0300

    refactor: app.js revised.

--- app.js
+++ app.js
@@ -9,8 +9,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(require('./routers/authRouter'));
+app.use(require('./routers/reviewRouter'));
+app.use(require('./routers/messageRouter'));
+app.use(require('./routers/discussionRouter'));
+app.use(require('./routers/listtypeRouter'));
+app.use(require('./routers/listsRouter'));
+app.use(require('./routers/userRouter'));
 
-//AutoDeployTest2
+//AutoDeployTest
 app.get('/',(req,res) =>{
     res.json("Auth Example Project");
 });
