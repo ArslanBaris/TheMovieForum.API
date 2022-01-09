d6ecb0af256fe382277f4bbb3b0207c978d42106
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-10 02:30:31 +0300

    feat: userRouter added.

---
+++ routers/userRouter.js
@@ -0,0 +1,20 @@
+const UserTrancactions = require("../database/userTransaction");
+
+c
+const userTrancactions = new UserTrancactions();
+
+const router = require("express")();
+const verifyToken = require("../middleware/verifyToken");
+
+router.get("/user/:Id", verifyToken, async (req, res) => {
+  const users = await userTrancactions.getUserById(req.params.Id);
+  res.json(users);
+});
+
+router.get("/userAll", verifyToken, async (req, res) => {
+  const users = await userTrancactions.getAllUsers(req.body);
+  res.json(users);
+});
+
+
+module.exports = router;
