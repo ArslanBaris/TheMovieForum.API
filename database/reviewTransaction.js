3fc376cfd4e6e18e1e2fcb2341175fce839658c7
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-10 21:28:55 +0300

    refactor: ReviewTrancactions revised.

--- database/reviewTransaction.js
+++ database/reviewTransaction.js
@@ -20,11 +20,49 @@ class ReviewTrancactions extends FadabHelper {
     console.log("Call GetUserReview!");
     return queryAsync(`SELECT * FROM Reviews WHERE UserId=${UserId}` );
   }
+
   getMovieReviews(movieId) {
     console.log("Call GetUserReview!");
     return queryAsync("SELECT * FROM ReviewUser WHERE MovieId=? ", movieId);
   }
+
+  addReview(values) {
+    const body_data = [
+      values.UserId,
+      values.Title,
+      values.MovieId,
+      values.MessageText,
+      values.CreatedDate,
+    ];
+    console.log("CallAddNewReview!");
+    return queryAsync(
+      "INSERT INTO Reviews (UserId,Title,MovieId,MessageText,CreatedDate) VALUES (?,?,?,?,?) ",
+      body_data
+    );
+  }
+
+  updateReview(values) {
+    const body_data = [
+      values.UserId,
+      values.Title,
+      values.MovieId,
+      values.MessageText,
+      values.CreatedDate,
+      values.Id,
+    ];
+    console.log("Updating review was successfully");
+    return queryAsync(
+      "UPDATE Reviews SET UserId = ?, Title = ?, MovieId = ?, MessageText = ?,  CreatedDate = ? WHERE Id = ?",
+      body_data
+    );
+  }
   
+  deleteReview(Id) {
+    console.log("Deleting This Review !");
+    return queryAsync("DELETE FROM Reviews WHERE Id = ? ", Id);
+  }
+ 
+
 }
 
 module.exports = ReviewTrancactions;
