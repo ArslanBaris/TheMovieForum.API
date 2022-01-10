fe442a361b1a77fba9f3215273ad6fc9804a7823
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-10 21:31:53 +0300

    refactor: ListTrancactions revised.

--- database/listsTransaction.js
+++ database/listsTransaction.js
@@ -20,6 +20,39 @@ class ListTrancactions extends FadabHelper {
     console.log("Call GetUserList!");
     return queryAsync(`SELECT * FROM Lists WHERE UserId=${UserId} and ListType = ${typeNumber} `);
   }
+
+  addList(values) {
+    const body_data = [values.ListType, values.UserId, values.MovieId];
+    console.log("CallAddNewList!");
+    return queryAsync(
+      "INSERT INTO Lists (ListType,UserId,MovieId) VALUES (?,?,?) ",
+      body_data
+    );
+  }
+  
+  updateList(values) {
+    const body_data = [
+      values.ListType,
+      values.UserId,
+      values.MovieId,
+      values.Id,
+    ];
+    console.log("Updating List was successfully");
+    return queryAsync(
+      "UPDATE Lists SET ListType = ?, UserId = ?, MovieId = ? WHERE Id = ?",
+      body_data
+    );
+  }
+
+  deleteList(UserId,typeNumber,movieId) {
+    console.log("Deleting This List !");
+    return queryAsync(`DELETE FROM Lists WHERE UserId=${UserId} and ListType = ${typeNumber}   and MovieId = ${movieId}`);
+  }
+  
+  checkUserMovieOnList(UserId,typeNumber,movieId) {
+    console.log("Call checkUserMovieOnList!");
+    return queryAsync(`SELECT * FROM Lists WHERE UserId=${UserId} and ListType = ${typeNumber}   and MovieId = ${movieId}`);
+  }
 }
 
 module.exports = ListTrancactions;
