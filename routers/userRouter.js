0874f46bb62a9195b4a9ac69ce7f68fd3a47cf2d
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-11 00:57:30 +0300

    refactor: userRouter revised.

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -14,5 +14,31 @@ router.get("/userAll", verifyToken, async (req, res) => {
   res.json(users);
 });
 
+router.get("/verifyUserName/:UserName", async (req, res) => {
+  const userName = await userTrancactions.getByUserName(req.params.UserName);
+  if(Object.keys(userName).length!==0)
+  res.send({"isExist":true})
+  else
+  res.send({"isExist":false})
+});
+
+router.get("/verifyEmail/:Email", async (req, res) => {
+  const email = await userTrancactions.getByEmail(req.params.Email);
+  if(Object.keys(email).length!==0)
+    res.send({"isExist":true})    
+  else
+  res.send({"isExist":false})  
+});
+
+router.put("/user", verifyToken, async (req, res) => {
+  const users = await userTrancactions.updateUser(Object.assign(req.body));
+  res.json(users);
+});
+
+
+router.delete("/user/:Id", verifyToken, async (req, res) => {
+  const users = await userTrancactions.deleteUser(req.params.Id);
+  res.json(users);
+});
 
 module.exports = router;
