38493958fc46faa189c7a52f967d3e4e5b5c36fa
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-10 18:24:46 +0300

    refactor: UserTrancactions revised.

--- database/userTransaction.js
+++ database/userTransaction.js
@@ -15,6 +15,54 @@ class UserTrancactions  extends FadabHelper{
     console.log("CallGetallusers!");
     return queryAsync("SELECT * FROM Users");
   }
+
+  addUser(values) {
+    const body_data = [
+      values.UserName,
+      values.FirstName,
+      values.LastName,
+      values.Email,
+      values.Password,
+      values.AvatarUrl,
+    ];
+    console.log("CallAddNewUser!");
+    return queryAsync(
+      "INSERT INTO Users (UserName,FirstName,LastName,Email,Password,AvatarUrl) VALUES (?,?,?,?,?,?) ",
+      body_data
+    );
+  } 
+  updateUser(values) {
+    const body_data = [
+      values.UserName,
+      values.FirstName,
+      values.LastName,
+      values.Email,
+      values.Password,
+      values.AvatarUrl,
+      values.Id,
+    ];
+    console.log("Updating was successfully");
+    return queryAsync(
+      "UPDATE Users SET UserName = ?, FirstName = ?, LastName = ?, Email = ?, Password = ?, AvatarUrl = ? WHERE Id = ?",
+      body_data
+    );
+  }
+
+  deleteUser(Id) {
+    console.log("Deleting This User !");
+    return queryAsync("DELETE FROM Users WHERE Id = ? ", Id);
+  }
+
+  
+  getByUserName(userName) {
+    console.log("Call GetUserName!", userName);
+    return queryAsync("SELECT * FROM Users WHERE UserName = ? ", userName);
+  } 
+
+  getByEmail(eMail) {
+    console.log("Call GetUserEmail!", eMail);
+    return queryAsync("SELECT * FROM Users WHERE Email = ? ", eMail);
+  }
 }
 
 module.exports = UserTrancactions;
