fa24e8b27d2f6b174645e483376ec1420cac742a
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-11 00:58:46 +0300

    refactor: reviewRouter revised.

--- routers/reviewRouter.js
+++ routers/reviewRouter.js
@@ -18,11 +18,25 @@ router.get("/movieReviewAll/:movieId", verifyToken, async (req, res) => {
   res.json(results);
 });
 
-
 router.get("/user/reviews/:userId", verifyToken, async (req, res) => {
   const results = await reviewTransactions.getUserReviews(
     Object.assign(req.params.userId)
   );
+  res.json(results);
+});
+
+router.post("/review", verifyToken, async (req, res) => {
+  const results = await reviewTransactions.addReview(req.body);
+  res.json(results);
+});
+
+router.put("/review", verifyToken, async (req, res) => {
+  const results = await reviewTransactions.updateReview(req.body);
+  res.json(results);
+});
+
+router.delete("/review/:Id", verifyToken, async (req, res) => {
+  const results = await reviewTransactions.deleteReview(req.params.Id);
   res.json(results);
 });
 
