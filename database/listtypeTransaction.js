9943fd5ec418893748f1cdb44df1156963a4b818
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-10 21:42:37 +0300

    refactor: ListTypeTrancactions revised

--- database/listtypeTransaction.js
+++ database/listtypeTransaction.js
@@ -17,6 +17,26 @@ class ListTypeTrancactions extends FadabHelper {
     return queryAsync("SELECT * FROM ListType");
   }
 
+  addListType(values) {
+    const body_data = [values.TypeName];
+    console.log("CallAddNewListType!");
+    return queryAsync("INSERT INTO ListType (TypeName) VALUES (?) ", body_data);
+  }
+
+  updateListType(values) {
+    const body_data = [values.TypeName, values.Id];
+    console.log("Updating ListType was successfully");
+    return queryAsync(
+      "UPDATE ListType SET TypeName = ?  WHERE Id = ?",
+      body_data
+    );
+  }
+
+  deleteListType(Id) {
+    console.log("Deleting This ListType !");
+    return queryAsync("DELETE FROM ListType WHERE Id = ? ", Id);
+  }
+
 }
 
 module.exports = ListTypeTrancactions;
